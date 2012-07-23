8e47a50a3b42c1fbc1baf2f93f248af3bd6c85ba
Keiichiro Ono <keiono@gmail.com>
2012-07-22 17:52:05 -0700

    Test template added.

---
+++ src/test/java/org/cytoscape/rserve/RServeConnectionTest.java
@@ -0,0 +1,23 @@
+package org.cytoscape.rserve;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RServeConnectionTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testConnection() {
+	}
+
+}
